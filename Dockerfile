9cc27a76c173c33764abff25b5045535898ab1af
Tal <118768187+tal-mat@users.noreply.github.com>
2024-09-29 19:21:23 +0300

    Update .gitignore and remove unwanted files from tracking

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:20
-
-COPY . /BlogBackEnd
-WORKDIR /BlogBackEnd
-
-RUN npm ci
-RUN npm run build
-
-CMD node dist/Index.js
--- dist/middlewares/activityLogger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const activityLogger = (req, res, next) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${req.method} ${req.path}`);
-    next();
-};
-exports.default = activityLogger;
